47962dfcefafeb5f614045cfa7d98db132c213e9
Giovanni <giovanni.padilha@outlook.com>
2025-07-06 21:54:58 -0300

    Adding some comments

--- src/dundie_api/main.py
+++ src/dundie_api/main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI(
     title="dundie-api",
     version="0.1.0",
-    description="dundie-api is a API for Dundie Rewards CLI Project"
+    description="dundie-api is a API for Dundie Rewards CLI Project."
 )
