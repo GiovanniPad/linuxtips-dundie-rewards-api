e7bb246c17ca7e427f39452e73f27fc1f61eb9cc
Giovanni Padilha <giovanni.padilha@outlook.com>
2025-07-15 07:20:44 -0300

    Day 4(config): Configuring the application
    Using Dynaconf to configure the application with environments
    variables and files, making possible the use of files to
    declare config variables.
    Exclude .vscode from git repository

---
+++ src/dundie_api/config.py
@@ -0,0 +1,30 @@
+"""Settings module"""
+# Biblioteca para manipular rotinas do Sistema Operacional.
+import os
+# Biblioteca para configuração.
+from dynaconf import Dynaconf # type: ignore
+
+# Armazena o caminho absoluto deste arquivo.
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+# Declarando a variável de configurações, através dela vai ser possível
+# importar acessar as variáveis de ambiente ou de arquivos.
+settings = Dynaconf(
+    # Definindo o prefixo para as variáveis de ambiente, apenas variáveis de
+    # ambiente com este prefixo (em maiúsculo) vão ser lidas.
+    envvar_prefix="dundie",
+    # Indica o arquivo que configuração padrão que vai ser carregado
+    # automaticamente pela aplicação.
+    preload=[os.path.join(HERE, "default.toml")],
+    # Indica outros arquivos que podem conter configurações necessárias.
+    settings_files=["settings.toml", ".secrets.toml"],
+    # Define múltiplos ambientes, podendo separar configurações para diferentes
+    # situações, como de produção ou desenvolvimento.
+    environments=["development", "production", "testing"],
+    # Define a variável de ambiente 'dundie_env' como a variável responsável
+    # por alterar qual ambiente vai usar.
+    env_switcher="dundie_env",
+    # Impede o carregamento de variáveis de ambiente de um arquivo .env
+    # se for True, a biblioteca vai tentar importar deste arquivo.
+    load_dotenv=False,
+)
