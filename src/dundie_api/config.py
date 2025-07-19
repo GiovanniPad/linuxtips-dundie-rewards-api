ad13278b9277390fe028589d609d6cc3c9829c46
Giovanni Padilha <giovanni.padilha@outlook.com>
2025-07-18 21:54:48 -0300

    Day 4(sec): Configuring the secret key
    Creating the configuration and validation to use secret keys for
    generate hashes and access tokens.

--- src/dundie_api/config.py
+++ src/dundie_api/config.py
@@ -2,7 +2,7 @@
 # Biblioteca para manipular rotinas do Sistema Operacional.
 import os
 # Biblioteca para configuração.
-from dynaconf import Dynaconf # type: ignore
+from dynaconf import Dynaconf, Validator # type: ignore
 
 # Armazena o caminho absoluto deste arquivo.
 HERE = os.path.dirname(os.path.abspath(__file__))
@@ -27,4 +27,12 @@ settings = Dynaconf(
     # Impede o carregamento de variáveis de ambiente de um arquivo .env
     # se for True, a biblioteca vai tentar importar deste arquivo.
     load_dotenv=False,
+    # Lista de validadores para realizar validações em cima de variáveis de configuração.
+    validators=[
+        # Validador que verifica se a chave 'SECRET_KEY' existe dentro da sessão
+        # 'SECURITY' dentro dos arquivos de configuração ou nas variáveis de ambiente.
+        # Ele também define que essa chave deve ser do tipo string e seu tamanho mínimo
+        # deve ser de 64 caracteres.
+        Validator("SECURITY__SECRET_KEY", must_exist=True, is_type_of=str, len_min=64)
+    ]
 )
